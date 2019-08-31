baa1af3a82d8e8b1d9c926b9f30b50ac785a990b
Denali Balser <denali.balser167@gmail.com>
2019-08-31 17:05:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,29 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = 'pending'
+    @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if sender.balance < @amount 
+      @status = 'rejected'
+      return "Transaction rejected. Please check your account balance."
+    elsif @status == 'complete'
+      "Transaction was already executed"
+    else
+      @sender.deposit( @amount * -1 )
+      @receiver.deposit( @amount )
+      @status = "complete"
+    end
+  end
+
 end
